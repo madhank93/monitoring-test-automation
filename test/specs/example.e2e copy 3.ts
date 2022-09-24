37aa6baa946d568c7deeb83ca705ceead57e3ef4
madhank93 <madhankumaravelu93@gmail.com>
2022-09-24 21:19:38 +0530

    adding tests source files

---
+++ test/specs/example.e2e copy 3.ts
@@ -0,0 +1,19 @@
+import Random from "../util/random.time";
+
+describe("Dummy test - 03", () => {
+  it("dummy test exists after a random time - 01", async () => {
+    await Random.getRandomTime();
+  });
+  it("dummy test exists after a random time - 02", async () => {
+    await Random.getRandomTime();
+  });
+  it("dummy test exists after a random time - 03", async () => {
+    await Random.getRandomTime();
+  });
+  it("dummy test exists after a random time - 04", async () => {
+    await Random.getRandomTime();
+  });
+  it("dummy test exists after a random time - 05", async () => {
+    await Random.getRandomTime();
+  });
+});
